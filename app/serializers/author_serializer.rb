2f8167cbccf9fd6d4c3a9ff2036f8c88a3b4d352
Bruno <brunochelimo@gmail.com>
2023-03-19 20:40:01 +0300

    done

---
+++ app/serializers/author_serializer.rb
@@ -0,0 +1,7 @@
+class AuthorSerializer < ActiveModel::Serializer
+  attributes :name, :profile, :posts
+
+  has_many :posts, serializer:AuthorPostSerializer
+  has_one :profile, serializer:ProfileSerializer
+
+end
